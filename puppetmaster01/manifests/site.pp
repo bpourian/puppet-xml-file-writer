798e430f8ad9ab1b8494b003c1280cb165066d90
bpourian <b.pourian@hotmail.co.uk>
2018-06-13 17:42:19 +0100

    added routing to site manifest

---
+++ puppetmaster01/manifests/site.pp
@@ -0,0 +1,18 @@
+stage { 'first':
+  before  => Stage['main'],
+}
+
+stage { 'last': }
+
+Stage['main'] -> Stage['last']
+
+node /^puppet/ {
+  class{'puppetmaster':
+    stage => 'main',
+  }
+
+  class{'webserver':
+    stage => 'last',
+  }
+
+}
